ded181f70b378096d3444896ff5a80f656c2f5d8
natnaelmax <natnaelhabtamu1998@gmail.com>
2026-06-14 20:53:44 +0300

    Add Higher_order_function and other scripts

--- Higher_order_function.py
+++ Higher_order_function.py
@@ -1,50 +1,48 @@
-# def square(x):
-#    return x**2
-# def cube(x):
-#    return x**3
-# def absolute(x):
-#    if x>=0:
-#       return x
-#    else:
-#       return -(x)
-# def higher_order_function(type):
-#    if type=="square":
-#       return square
-#    elif type=="cube":
-#       return cube
-#    else:
-#       return absolute
-# result=higher_order_function("square")
-# print(result(3))
-# result=higher_order_function("cube")
-# print(result(3))
-# result=higher_order_function("absolute")
-# print(result(-6))
-# # closure
-# def add_five():
-#    five=5
-#    def add(num):
-#       return five+num
-#    return add
-# closure=add_five()
-# print(closure(10))
-# # decorator
-# def greeting():
-#    return "welcome to python"
-# def decorator_uppercase(function):
-#    def wrapper():
-#       func=function()
-#       make_uppercase=func.upper()
-#       return make_uppercase
-#    return wrapper
-# res=decorator_uppercase(greeting)
-# print(res())
+def square(x):
+   return x**2
+def cube(x):
+   return x**3
+def absolute(x):
+   if x>=0:
+      return x
+   else:
+      return -(x)
+def higher_order_function(type):
+   if type=="square":
+      return square
+   elif type=="cube":
+      return cube
+   else:
+      return absolute
+result=higher_order_function("square")
+print(result(3))
+result=higher_order_function("cube")
+print(result(3))
+result=higher_order_function("absolute")
+print(result(-6))
+# closure
+def add_five():
+   five=5
+   def add(num):
+      return five+num
+   return add
+closure=add_five()
+print(closure(10))
+# decorator
+def greeting():
+   return "welcome to python"
+def decorator_uppercase(function):
+   def wrapper():
+      func=function()
+      make_uppercase=func.upper()
+      return make_uppercase
+   return wrapper
+res=decorator_uppercase(greeting)
+print(res())
 
-numbers = [1, 2, 3, 4, 5] # iterable
+numbers = [1, 2, 3, 4, 5] 
 def square(x):
     return x ** 2
 numbers_squared = map(square, numbers)
 print(list(numbers_squared))   
 
-# numbers_squared = map(lambda x : x ** 2, numbers)
-# print(list(numbers_squared))    
